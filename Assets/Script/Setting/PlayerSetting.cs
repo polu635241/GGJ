aa9729648bb31ef4d17a0bf0f2b2324237722bc2
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-01 03:01:26 +0800

    新增玩家設定檔

---
+++ Assets/Script/Setting/PlayerSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Setting/PlayerSetting")]
+public class PlayerSetting : ScriptableObject 
+{
+	[SerializeField]
+	float moveSpeed;
+
+	public float MoveSpeed
+	{
+		get
+		{
+			return moveSpeed;
+		}
+	}
+
+	[SerializeField]
+	float jumpForce;
+
+	public float JumpForce
+	{
+		get
+		{
+			return jumpForce;
+		}
+	}
+}
