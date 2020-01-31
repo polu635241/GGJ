1d8c1b3f5eec6c1476fe38fa7c0410cee4d391cb
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-01 01:40:26 +0800

    新增基礎的玩家狀態機系統

---
+++ Assets/Script/Player/PlayerFSM/PlayerFlowState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerFlowState
+{
+	protected PlayerFlowController flowController;
+	protected int backgroundSensingLayerMask;
+	
+	public PlayerFlowState (PlayerFlowController playerFlowController)
+	{
+		this.flowController = playerFlowController;
+	}
+	
+	public virtual void Enter(PlayerFlowState prevState)
+	{
+		
+	}
+	
+	public virtual PlayerFlowState Stay (float deltaTime)
+	{
+		return null;
+	}
+	
+	public virtual void Exit()
+	{
+		
+	}
+	
+	protected abstract string BindAnimationName{ get;}
+	
+	protected PlayerFlowState GetState<T> () where T:PlayerFlowState
+	{
+		return flowController.GetState<T> ();
+	}
+}
---
+++ Assets/Script/Player/PlayerFSM/States/PlayerIdleState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerFlowState {
+	
+	public PlayerIdleState (PlayerFlowController playerFlowController) : base (playerFlowController)
+	{
+		
+	}
+	
+	public override void Enter (PlayerFlowState prevState)
+	{
+		base.Enter (prevState);
+	}
+	
+	public override PlayerFlowState Stay (float deltaTime)
+	{
+		return null;
+	}
+
+	protected override string BindAnimationName 
+	{
+		get 
+		{
+			return AnimationNames.Idle;
+		}
+	}
+}
