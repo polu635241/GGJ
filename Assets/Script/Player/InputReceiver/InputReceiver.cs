adc99b9ac2ca1d3ae1095550ac128333dcc27212
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-01 01:50:36 +0800

    新增抽象的輸入接收器型別 並實作左玩家輸入接收器型別

---
+++ Assets/Script/Player/InputReceiver/InputReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputReceiver
+{
+	public abstract bool Right ();
+
+	public abstract bool Left ();
+
+	public abstract bool Up ();
+
+	public abstract bool Down ();
+
+	public abstract bool Jump ();
+}
---
+++ Assets/Script/Player/InputReceiver/LeftPlayerInputReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPlayerInputReceiver : InputReceiver 
+{
+	public override bool Right ()
+	{
+		return Input.GetKey (KeyCode.D);
+	}
+
+	public override bool Left ()
+	{
+		return Input.GetKey (KeyCode.A);
+	}
+
+	public override bool Up ()
+	{
+		return Input.GetKey (KeyCode.W);
+	}
+
+	public override bool Down ()
+	{
+		return Input.GetKey (KeyCode.S);
+	}
+
+	public override bool Jump ()
+	{
+		return Input.GetKey (KeyCode.Space);
+	}
+}
