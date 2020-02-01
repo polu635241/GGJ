1d98742543fa08e3503bf169649e037682b00388
polu_kun <polu635241@gmail.com>
2020-02-01 13:43:48 +0800

    修改物理設定以及圖層管理

--- Assets/Script/Consts/Layer.cs
+++
@@ -1,6 +0,0 @@
-﻿public static class Layer
-{
-	public const string Player = "Player";
-	public const string Plus = "Plus";
-	public const string Crossable = "Crossable";
-}
---
+++ Assets/Script/Consts/Layers.cs
@@ -0,0 +1,8 @@
+﻿public static class Layers
+{
+	public const string Player = "Player";
+	public const string Plus = "Plus";
+	public const string Crossable = "Crossable";
+	public const string PlusSensor = "PlusSensor";
+	public const string AttachPlus = "AttachPlus";
+}
