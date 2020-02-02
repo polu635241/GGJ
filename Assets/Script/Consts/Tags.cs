8d4eeb6269982c431c6c58c11e9caf469e29f469
polu_kun <polu635241@gmail.com>
2020-02-02 11:32:59 +0800

    新增攝影機控系統

--- Assets/Script/Consts/Tags.cs
+++ Assets/Script/Consts/Tags.cs
@@ -7,4 +7,5 @@ public class Tags
 	public const string PlusRoot = "PlusRoot";
 	public const string FightFx = "FightFx";
 	public const string TimeClockText = "TimeClockText";
+	public const string tagUI = "tagUI";
 }
